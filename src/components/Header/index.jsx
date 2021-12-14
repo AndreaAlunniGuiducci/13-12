421c3bf9f24b5dfa08b73664df7cfe3248f9c284
Andrea <andrea.aguiducci@gmail.com>
2021-12-14 11:30:53 +0100

    feat: header & footer

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from './Header.module.scss';
+
+const Header = (props) => {
+    const name = props.name || 'App';
+    const links = props.links || [{ link: '/', label: 'link' }];
+
+    return (
+        <header className={styles.header}>
+            <h1>{name}</h1>
+            <nav>
+                <ul>
+                    {links.map((item, index) =>
+                        <li key={index}><a href={item.link}>{item.label}</a></li>)}
+                </ul>
+            </nav>
+        </header >
+    );
+}
+
+export default Header;
